40f0bb92d39212a98dbd71ec7af96f0979caaa31
Sambhav <stanay929@gmail.com>
2026-09-05 12:27:12 +0530

    main_update - 1

--- Project/main.py
+++ Project/main.py
@@ -15,3 +15,17 @@
 #    ├── planning
 #    │
 #    └── control robot
+
+from camera import create_world, get_camera_image
+from perception.obstacle_detection import detect_obstacles
+from perception.free_space import detect_free_space
+
+ugv = create_world()
+
+while True:
+
+    frame = get_camera_image(ugv)
+
+    obstacles = detect_obstacles(frame)
+
+    free_space = detect_free_space(frame)
